2a4e9468b0ebf25a1064a0e0945fa953a6267e12
Christian Guizar <c-guizar@hotmail.com>
2021-08-27 11:56:41 -0700

    feat(header): correction

---
+++ candy.js
@@ -0,0 +1,10 @@
+
+const spanSpam = () => {
+  for (let i = 1; i < 7; i++) {
+    let itm = document.getElementById("candySpan");
+    const cln = itm.cloneNode(true);
+    document.getElementById("headingGroup").appendChild(cln)
+  }
+}
+
+spanSpam()
