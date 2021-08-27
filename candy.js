4bedd9c016d0a872d88773ea210e8e6e5edaca2e
Christian Guizar <c-guizar@hotmail.com>
2021-08-27 13:47:15 -0700

    feat(header): clarification

--- candy.js
+++ candy.js
@@ -3,8 +3,6 @@ const spanSpam = () => {
   for (let i = 1; i < 7; i++) {
     let itm = document.getElementById("candySpan");
     const cln = itm.cloneNode(true);
-    document.getElementById("headingGroup").appendChild(cln)
+    document.getElementById("headingGroup").appendChild(cln);
   }
 }
-
-spanSpam()
